7db123cc4d9daecdde293224138a1127fc3f7e56
Emilio Hernandez <emilio25informatic@gmail.com>
2023-02-11 17:13:24 -0300

    Prompt, confirm, alert

---
+++ 01-fundamentals/assets/js/alerts.js
@@ -0,0 +1,12 @@
+alert('Hola mundo')
+
+let nombre = prompt('¿Cual es tu nombre?', 'Sin nombre')
+
+console.log(nombre)
+console.log('****' + nombre +'****')
+
+const selection = confirm('Está seguro de borrar esto?')
+
+console.log(selection)
+
+console.log(global)
