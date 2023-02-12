c3082ebdce5ddefeed024f20174806bfbc8e686e
Emilio Hernandez <emilio25informatic@gmail.com>
2023-02-12 12:45:48 -0300

    ✨ General introduction to primitive types

---
+++ 01-fundamentals/assets/js/primitivos.js
@@ -0,0 +1,34 @@
+let fullName = 'Brendan Eich'
+console.log(fullName)
+
+fullName = 'Taylor Otwell'
+console.log(fullName)
+
+fullname = "Miško Hevery"
+fullName = `Kamil Mysliwiec`
+
+console.log(typeof fullName)
+fullName = 29
+
+console.log(typeof fullName)
+let isFlag = false
+console.log(typeof isFlag)
+
+let age = 34
+console.log(typeof age)
+
+let price = 500.5
+console.log(typeof price)
+
+
+let superPoder
+console.log(typeof superPoder)
+
+let iAmNull = null
+console.log(typeof iAmNull)
+
+let symbol1 = Symbol('a')
+let symbol2 = Symbol('a')
+console.log(typeof symbol1)
+
+console.log(symbol1 === symbol2)
