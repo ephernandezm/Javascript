91513faa78b2f04e339ed7aef7482d9fc9c59668
Emilio Hernandez <emilio25informatic@gmail.com>
2023-02-15 09:51:40 -0300

    ✨ Arrays

---
+++ 01-fundamentals/assets/js/arrays.js
@@ -0,0 +1,30 @@
+// const arr = new Array(10)
+// const arr = []
+// console.log(arr)
+
+let videoGame = ['Mario 3', 'Megaman', 'Chrono Trigger']
+console.log({ videoGame })
+
+console.log(videoGame[0])
+
+let arraysOfThings = [
+    true,
+    123,
+    'Emilio',
+    1 + 2,
+    function() {},
+    () => {},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr. Willy',
+        'Woodman',
+    ]]
+]
+
+// console.log({ arraysOfThings })
+// console.log(arraysOfThings[0])
+// console.log(arraysOfThings[2])
+
+// console.log(arraysOfThings[7][3])
+console.log(arraysOfThings[7][4][1])
+
