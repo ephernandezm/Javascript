87bdd1bde01dd14d25ffbb8d5e84d98f0a6309cf
Emilio Hernandez <emilio25informatic@gmail.com>
2023-02-20 17:47:45 -0300

    ✅ Return of functions

--- 01-fundamentals/assets/js/funciones.js
+++ 01-fundamentals/assets/js/funciones.js
@@ -1,6 +1,10 @@
 function greet(name) {
-    console.log(arguments)
-    console.log(`Hello ${name}`)
+    // console.log(arguments)
+    // console.log(`Hello ${name}`)
+    return [1, 2, 3, 4, 5]
+
+    // This will never run
+    console.log('I am a code that is after the return')
 }
 
 const greetTwo = function (name) {
@@ -11,6 +15,21 @@ const greetArrow = (name) => {
     console.log(`Hello ${name}`)
 }
 
-greet('Emilio', 34, true, 'Venezuela')
-greetTwo('Emiliano')
-greetArrow('Maria')
+const returnToSayHello = greet('Emilio', 34, true, 'Venezuela')
+
+// greetTwo('Emiliano')
+// greetArrow('Maria')
+
+function addTwoNumbers(numberOne, numberTwo) {
+    return numberOne + numberTwo
+}
+
+const addTwoNumbersArrowFunction = (numberOne, numberTwo) => numberOne + numberTwo
+
+function getRandomNumber() {
+    return Math.random()
+}
+
+const getRandomNumberArrowFunction = () => Math.random()
+
+console.log(getRandomNumberArrowFunction())
