847594f09c0e421da3a42a5f43b96237ad50ae91
Emilio Hernandez <emilio25informatic@gmail.com>
2023-02-23 09:22:46 -0300

    ✅ Alternative to if else

--- 01-fundamentals/assets/js/if-else.js
+++ 01-fundamentals/assets/js/if-else.js
@@ -30,3 +30,9 @@ if (day === 0) {
 } else {
     console.log("It's not Monday, Tuesday or Sunday")
 }
+
+day = 6 // 0: Sunday, 1: Monday, 2: Tuesday, 3: Wednesday, 4: Thursday, 5: Friday, 6: Saturday
+
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',]
+
+console.log(weekdays[day] || 'Undefined day')
